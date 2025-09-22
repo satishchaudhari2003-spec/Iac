a70525238470e1a041038cb998c6dffe2e43ed79
satishchaudhari2003-spec <satishchaudhari2003@gmail.com>
2025-09-22 12:42:27 +0530

    added auto config

--- eks-auto-terraform-cicd/modules/eks-auto/main.tf
+++ eks-auto-terraform-cicd/modules/eks-auto/main.tf
@@ -2,16 +2,16 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "21.1.0"
 
-  cluster_name    = var.cluster_name
-  cluster_version = var.cluster_version
-  subnet_ids      = var.subnet_ids
-  vpc_id          = var.vpc_id
-
-  enable_cluster_creator_admin_permissions = true
-
-  eks_managed_node_groups = {}
-  eks_auto_scaling_groups = {}
-  eks_auto_mode           = true
+  cluster_name       = var.cluster_name
+  cluster_version    = var.kubernetes_version
+  subnet_ids         = var.subnet_ids
+  cluster_role_arn   = var.cluster_role_arn
+  vpc_id             = var.vpc_id
+  
+  cluster_compute_config = {
+    enabled = true   # Enable EKS Auto Mode here
+  }
 
   tags = var.tags
 }
+
